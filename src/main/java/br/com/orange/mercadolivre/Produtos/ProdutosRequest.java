ff7048ebab625c697746523d771d812c59cb5fc1
Kaiofprates <kaioprates@outlook.com>
2021-02-23 18:03:30 -0300

    validação inical para categorias e caracteristicas

---
+++ src/main/java/br/com/orange/mercadolivre/Produtos/ProdutosRequest.java
@@ -0,0 +1,43 @@
+package br.com.orange.mercadolivre.Produtos;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+
+public class ProdutosRequest {
+
+    private String nome;
+    private BigDecimal valor;
+    private String descricao;
+    private Long categoria;
+    private LocalDateTime registro = LocalDateTime.now(ZoneId.systemDefault());
+    private List<Caracteristicas> caracteristicas ;
+
+    public ProdutosRequest(String nome, BigDecimal valor, String descricao, Long categoria, List<Caracteristicas> caracteristicas) {
+        this.nome = nome;
+        this.valor = valor;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.caracteristicas = caracteristicas;
+    }
+
+    @Override
+    public String toString() {
+        return "ProdutosRequest{" +
+                "nome='" + nome + '\'' +
+                ", valor=" + valor +
+                ", descricao='" + descricao + '\'' +
+                ", categoria=" + categoria +
+                ", caracteristicas=" + caracteristicas.size() +
+                '}';
+    }
+
+    public Long getCategoria() {
+        return categoria;
+    }
+
+    public List<Caracteristicas> getCaracteristicas() {
+        return caracteristicas;
+    }
+}
---
+++ src/test/java/br/com/orange/mercadolivre/Produtos/ProdutosTest.java
@@ -0,0 +1,17 @@
+package br.com.orange.mercadolivre.Produtos;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProdutosTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+}
