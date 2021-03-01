7e0c229f6b1e312040e51266bc8776c26547ce26
Kaiofprates <kaioprates@outlook.com>
2021-03-01 19:44:51 -0300

    [feature] cadastro de pergunta e envio de email

---
+++ src/main/java/br/com/orange/mercadolivre/Pergunta/PerguntaRequest.java
@@ -0,0 +1,43 @@
+package br.com.orange.mercadolivre.Pergunta;
+
+import br.com.orange.mercadolivre.Produtos.Produto;
+import br.com.orange.mercadolivre.Usuario.Usuario;
+import org.springframework.util.Assert;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+;import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class PerguntaRequest {
+
+    @NotBlank
+    private String titulo;
+    @NotNull
+    private LocalDateTime criacao = LocalDateTime.now(ZoneId.systemDefault());
+
+    public PerguntaRequest(@NotBlank String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Deprecated
+    public  PerguntaRequest(){}
+
+    @Override
+    public String toString() {
+        return "PerguntaRequest{" +
+                "titulo='" + titulo + '\'' +
+                ", criacao=" + criacao +
+                '}';
+    }
+
+    public Pergunta toModel(Usuario perguntador, Produto produto) {
+        Assert.notNull(perguntador, "O usuário inválido");
+        Assert.notNull(produto, "Não foi possível recuperar o produto informado");
+        return new Pergunta(titulo,criacao,perguntador,produto);
+    }
+}
