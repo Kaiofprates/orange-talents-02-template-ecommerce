7e3f36a20ef0d11d692f9fbf419185551af32ed2
Kaiofprates <kaioprates@outlook.com>
2021-03-05 14:12:10 -0300

    [refator] Adicionando variável de ambiente para recurso de email.

--- src/main/java/br/com/orange/mercadolivre/MailSender/SendMail.java
+++ src/main/java/br/com/orange/mercadolivre/MailSender/SendMail.java
@@ -2,6 +2,7 @@ package br.com.orange.mercadolivre.MailSender;
 
 import br.com.orange.mercadolivre.handlers.exceptions.MailSenderException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
@@ -11,12 +12,15 @@ public  class SendMail {
 
     @Autowired
     private JavaMailSender mailSender;
+    @Value("${spring.mail.username}")
+    private String email;
 
     public String send(String mens, String to){
+
         SimpleMailMessage message = new SimpleMailMessage();
         message.setText(mens);
         message.setTo(to);
-        message.setFrom("orangetalents3@gmail.com");
+        message.setFrom(email);
 
         try {
             mailSender.send(message);
